4207754779f52477b9be33c60b736a58728ac1b5
joy-rosie <joytolia@hotmail.com>
2022-04-19 23:25:38 +0100

    add userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 sudo apt update
 sudo apt install python3.9 -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
-git clone https://github.com/joy-rosie/wedbot.git
-cd wedbot
+git clone https://github.com/joy-rosie/wedbot.git /opt/wedbot
 python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-#write out current crontab
+python3.9 -m pip install -r /opt/wedbot/requirements.txt
+touch /opt/wedbot/.env
+echo 'TELEGRAM_BOT_TOKEN=""' >> /opt/wedbot/.env
+echo 'TELEGRAM_CHAT_ID=""' >> /opt/wedbot/.env
 crontab -l > mycron
-#echo new cron into cron file
-echo "* * * * * /usr/bin/python3.9 /home/ubuntu/wedbot/main.py" >> mycron
-#install new cron file
+echo "* * * * * /usr/bin/python3.9 /opt/wedbot/main.py" >> mycron
 crontab mycron
 rm mycron
+sudo service crond status
